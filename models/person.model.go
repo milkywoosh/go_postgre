db0c471ddae5e831b36afeb82f8998f7b7895a20
Auliya Lukman <archer.lukman@gmail.com>
2024-04-24 06:34:24 +0700

    set type to pointer type to handle null value from database

--- models/person.model.go
+++ models/person.model.go
@@ -6,11 +6,11 @@ import "time"
 //
 
 type Person struct {
-	ID         int       `json:"id_person,omitempty"`
-	NamePerson string    `json:"name_person,omitempty"`
-	SchoolID   int       `json:"school_id,omitempty"`
-	CreatedAt  time.Time `json:"created_at,omitempty"`
-	CreatedBy  int       `json:"created_by,omitempty"`
+	ID         *int       `json:"id_person,omitempty"`
+	NamePerson *string    `json:"name_person,omitempty"`
+	SchoolID   *int       `json:"school_id,omitempty"`
+	CreatedAt  *time.Time `json:"created_at" form:"created_at" time_format:"unix"`
+	CreatedBy  *int       `json:"created_by,omitempty"`
 }
 
 type CreatePerson struct{}
