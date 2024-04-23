2bfaed7aa1d7704c6ffa39e7496757165c434f7e
Auliya Lukman <archer.lukman@gmail.com>
2024-04-24 06:33:56 +0700

    modif subject info for avoid auto appear created_at

--- models/subject.model.go
+++ models/subject.model.go
@@ -1,14 +1,14 @@
 package models
 
 // note : paradigma TYPE STRUCT can be based on FIELD or CERTAIN METHOD
-//
 type Subject struct {
-	IdSubject   uint   `json:"id_subject,omitempty"`
-	SubjectName string `json:"subject_name,omitempty"`
-	IdPeople    uint   `json:"id_people_subject,omitempty"`
+	IdSubject   *int    `json:"id_subject,omitempty"`
+	SubjectName *string `json:"subject_name,omitempty"`
+	IdPeople    *int    `json:"id_people_subject,omitempty"`
 }
 
 type SubjectInfo struct {
-	Subject
-	Person
+	IdSubject   *int    `json:"id_subject,omitempty"`
+	NamePerson  *string `json:"name_person,omitempty"`
+	SubjectName *string `json:"subject_name,omitempty"`
 }
