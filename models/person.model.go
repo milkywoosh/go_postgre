b30d8a9a2a0daabb11ff9af6229f0fa1f6e8db7d
sigma.lukman <archer.lukman@gmail.com>
2024-04-29 16:18:29 +0700

    add Person model

--- models/person.model.go
+++ models/person.model.go
@@ -6,11 +6,17 @@ import "time"
 //
 
 type Person struct {
-	ID         int       `json:"id,omitempty"`
-	NamePerson string    `json:"name_person,omitempty"`
-	SchoolID   int       `json:"school_id,omitempty"`
-	CreatedAt  time.Time `json:"created_at,omitempty"`
-	CreatedBy  int       `json:"created_by,omitempty"`
+	ID            int        `json:"id_person,omitempty"`
+	FirstName     string     `json:"first_name,omitempty"`
+	LastName      string     `json:"last_name,omitempty"`
+	Address       string     `json:"address,omitempty"`
+	PhoneNumber   string     `json:"phone_number,omitempty"`
+	Job           string     `json:"job,omitempty"`
+	InstagramName string     `json:"instagram_uname,omitempty"`
+	FBName        string     `json:"facebook_uname,omitempty"`
+	PostalCode    string     `json:"postal_code,omitempty"`
+	CreatedAt     *time.Time `json:"created_at,omitempty"`
+	IDUser        int        `json:"id_user,omitempty"`
 }
 
 type CreatePerson struct{}
