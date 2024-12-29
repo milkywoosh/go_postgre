717a248e1b4c2145f7f98a39b752c3da8e0a8661
sigma.lukman <archer.lukman@gmail.com>
2024-12-30 06:49:56 +0700

    move type struct to model

--- repositories/books.repo.go
+++ repositories/books.repo.go
@@ -26,23 +26,14 @@ func (br BooksRepo) FetchBookByID(book_id string, ctx *gin.Context) (string, err
 	return book_name, nil
 }
 
-type TitleType string
-type PriceType float64
-type AuthorType string
-
-type BooksLike struct {
-	Title TitleType `json:"title"`
-	Price PriceType `json:"price"`
-}
-
-func (br BooksRepo) FetchBooksLikeName(name_like string, ctx *gin.Context) ([]BooksLike, error) {
+func (br BooksRepo) FetchBooksLikeName(name_like string, ctx *gin.Context) ([]models.BooksLike, error) {
 
 	query_books_like := fmt.Sprintf(`SELECT title, price FROM books b WHERE title LIKE %s`, "'%'||$1||'%'") // '%'||$1||'%'
 	fmt.Println(query_books_like)
 	var rows *sql.Rows
 	var err error
-	var eachResult BooksLike
-	var searchResults []BooksLike = make([]BooksLike, 0)
+	var eachResult models.BooksLike
+	var searchResults []models.BooksLike = make([]models.BooksLike, 0)
 
 	rows, err = br.DB.QueryContext(ctx, query_books_like, name_like)
 	if err != nil {
@@ -59,13 +50,7 @@ func (br BooksRepo) FetchBooksLikeName(name_like string, ctx *gin.Context) ([]Bo
 	return searchResults, nil
 }
 
-type BooksAuthorLike struct {
-	Title      TitleType       `json:"title"`
-	AuthorName AuthorType      `json:"author_name"`
-	CreatedAt  models.JSONTime `json:"author_registered_at"`
-}
-
-func (br BooksRepo) FetchBooksByAuthor(author_name_like string, ctx *gin.Context) ([]BooksAuthorLike, error) {
+func (br BooksRepo) FetchBooksByAuthor(author_name_like string, ctx *gin.Context) ([]models.BooksAuthorLike, error) {
 
 	query_books_like := fmt.Sprintf(`
 	SELECT a.author_name, b.title, DATE(a.created_at) FROM books b 
@@ -74,15 +59,14 @@ func (br BooksRepo) FetchBooksByAuthor(author_name_like string, ctx *gin.Context
 
 	var rows *sql.Rows
 	var err error
-	var eachResult BooksAuthorLike
-	var searchResults []BooksAuthorLike = make([]BooksAuthorLike, 0)
+	var eachResult models.BooksAuthorLike
+	var searchResults []models.BooksAuthorLike = make([]models.BooksAuthorLike, 0)
 
 	rows, err = br.DB.QueryContext(ctx, query_books_like, author_name_like)
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Println("rows", rows)
 	for rows.Next() {
 		if err = rows.Scan(&eachResult.AuthorName, &eachResult.Title, &eachResult.CreatedAt); err != nil {
 			return nil, err
