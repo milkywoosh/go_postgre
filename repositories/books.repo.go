edf1063d4d0e524d50bcbedda8ce9a0201049c6e
sigma.lukman <archer.lukman@gmail.com>
2024-12-31 01:12:11 +0700

    put repo inside service struct

--- repositories/books.repo.go
+++ repositories/books.repo.go
@@ -1,10 +1,10 @@
 package repositories
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 
-	"github.com/gin-gonic/gin"
 	"github.com/milkyway/gin_beginer/models"
 )
 
@@ -12,24 +12,35 @@ type BooksRepo struct {
 	DB *sql.DB
 }
 
-func (br BooksRepo) FetchBookByID(book_id int, ctx *gin.Context) (string, error) {
-	var book_name string
-	var rows *sql.Row
+func (br BooksRepo) FetchBookByID(ctx context.Context, book_id int) ([]models.Books, error) {
 
-	query_book_get_by_id := `SELECT title FROM books b where b.id = $1`
+	var book_info models.Books
+	var book_info_rows []models.Books
+	var rows *sql.Rows
+	var err error
 
-	rows = br.DB.QueryRowContext(ctx, query_book_get_by_id, book_id)
-	if err := rows.Scan(&book_name); err != nil {
-		return "", err
+	query_book_get_by_id := `SELECT title, stock_qty, price FROM books b where b.id = $1`
+	rows, err = br.DB.QueryContext(ctx, query_book_get_by_id, book_id)
+	if err != nil {
+		return nil, err
 	}
 
-	return book_name, nil
+	defer rows.Close() // must be called to prevent leaks connection!
+
+	for rows.Next() {
+		if err = rows.Scan(&book_info.BookName, &book_info.StockQty, &book_info.Price); err != nil {
+			return nil, err
+		}
+
+		book_info_rows = append(book_info_rows, book_info)
+	}
+
+	return book_info_rows, nil
 }
 
-func (br BooksRepo) FetchBooksLikeName(name_like string, ctx *gin.Context) ([]models.BooksLike, error) {
+func (br BooksRepo) FetchBooksLikeName(ctx context.Context, name_like string) ([]models.BooksLike, error) {
 
 	query_books_like := fmt.Sprintf(`SELECT title, price FROM books b WHERE title LIKE %s`, "'%'||$1||'%'") // '%'||$1||'%'
-	fmt.Println(query_books_like)
 	var rows *sql.Rows
 	var err error
 	var eachResult models.BooksLike
@@ -39,6 +50,8 @@ func (br BooksRepo) FetchBooksLikeName(name_like string, ctx *gin.Context) ([]mo
 	if err != nil {
 		return nil, err
 	}
+
+	defer rows.Close()
 
 	for rows.Next() {
 		if err = rows.Scan(&eachResult.Title, &eachResult.Price); err != nil {
@@ -50,7 +63,7 @@ func (br BooksRepo) FetchBooksLikeName(name_like string, ctx *gin.Context) ([]mo
 	return searchResults, nil
 }
 
-func (br BooksRepo) FetchBooksByAuthor(author_name_like string, ctx *gin.Context) ([]models.BooksAuthorLike, error) {
+func (br BooksRepo) FetchBooksByAuthor(ctx context.Context, author_name_like string) ([]models.BooksAuthorLike, error) {
 
 	query_books_like := fmt.Sprintf(`
 	SELECT a.author_name, b.title, DATE(a.created_at) FROM books b 
@@ -66,6 +79,8 @@ func (br BooksRepo) FetchBooksByAuthor(author_name_like string, ctx *gin.Context
 	if err != nil {
 		return nil, err
 	}
+
+	defer rows.Close()
 
 	for rows.Next() {
 		if err = rows.Scan(&eachResult.AuthorName, &eachResult.Title, &eachResult.CreatedAt); err != nil {
---
+++ services/books.service.go
@@ -0,0 +1,50 @@
+package services
+
+// note handle sisi business logic
+
+import (
+	"context"
+
+	"github.com/milkyway/gin_beginer/models"
+	"github.com/milkyway/gin_beginer/repositories"
+)
+
+// note: layer ini khusus bisnis logic, data received from REPO layer
+type BooksService struct {
+	BooksRepo repositories.BooksRepo
+}
+
+func (bs BooksService) GetBookInfo(ctx context.Context, book_id int) ([]models.Books, error) {
+
+	var book_info_rows []models.Books
+	var err error
+
+	book_info_rows, err = bs.BooksRepo.FetchBookByID(ctx, book_id)
+	if err != nil {
+		return nil, err
+	}
+	return book_info_rows, nil
+}
+
+func (bs BooksService) SearchLikeName(ctx context.Context, name_like string) ([]models.BooksLike, error) {
+	var search_info_rows []models.BooksLike
+	var err error
+
+	search_info_rows, err = bs.BooksRepo.FetchBooksLikeName(ctx, name_like)
+	if err != nil {
+		return nil, err
+	}
+	return search_info_rows, nil
+}
+
+func (bs BooksService) SearchBookByAuthor(ctx context.Context, author_name_like string) ([]models.BooksAuthorLike, error) {
+	var err error
+	var searchResults []models.BooksAuthorLike
+
+	searchResults, err = bs.BooksRepo.FetchBooksByAuthor(ctx, author_name_like)
+	if err != nil {
+		return nil, err
+	}
+
+	return searchResults, nil
+}
