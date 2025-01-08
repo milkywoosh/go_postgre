e2d2067d87b00c1346c49b8ec4dc496aff780cd2
sigma.lukman <auliya.lukman@sigma.co.id>
2025-01-08 23:24:52 +0700

    add route /get_roles/:username

--- routes/userRoles.route.go
+++ routes/userRoles.route.go
@@ -17,6 +17,7 @@ func (ru *RoleUsersRouteController) RoleUsersRoute(routerGroup *gin.RouterGroup)
 	router := routerGroup.Group("roleusers")
 
 	router.GET("/:user_id", ru.UserRolesController.GetRoleOfUser)
+	router.GET("/get_roles/:username", ru.UserRolesController.GetRolesByUsername)
 	router.POST("/assign_role", ru.UserRolesController.AssignRolesBeginTx)
 	router.DELETE("/delete_role", ru.UserRolesController.DeleteRoleBeginTx)
 }
