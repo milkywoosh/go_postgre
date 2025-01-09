9175794e21e162e0248d70586bfe76c7ee1303d7
sigma.lukman <auliya.lukman@sigma.co.id>
2025-01-09 07:44:16 +0700

    omit get user by ID

--- routes/userRoles.route.go
+++ routes/userRoles.route.go
@@ -16,7 +16,6 @@ func NewRouteUserRolesController(UserRolesController controllers.UserRolesContro
 func (ru *RoleUsersRouteController) RoleUsersRoute(routerGroup *gin.RouterGroup) {
 	router := routerGroup.Group("roleusers")
 
-	router.GET("/:user_id", ru.UserRolesController.GetRoleOfUser)
 	router.GET("/get_roles/:username", ru.UserRolesController.GetRolesByUsername)
 	router.POST("/assign_role", ru.UserRolesController.AssignRolesBeginTx)
 	router.DELETE("/delete_role_service", ru.UserRolesController.DeleteRoleServiceBeginTx)
