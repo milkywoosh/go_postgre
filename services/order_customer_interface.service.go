41cb00a39e0298e5e767c47e22f69ccd1b576be9
sigma.lukman <auliya.lukman@sigma.co.id>
2025-03-25 21:55:23 +0700

    implement interfacee OrderCustomer

--- services/order_customer_interface.service.go
+++ services/order_customer_interface.service.go
@@ -3,12 +3,13 @@ package services
 import "github.com/milkyway/gin_beginer/models"
 
 // get from UI
-type BookOrderInfo struct {
-	ID    string // string or int ??
-	Title string
-	Qty   int
+type OrderBookRequest struct {
+	ID    string `json:"id" binding:"required"` // string or int ??
+	Title string `json:"title" binding:"required"`
+	Qty   int    `json:"qty" binding:"required"`
 }
 
 type OrderCustomer interface {
-	BuyBooks(models.Books, []BookOrderInfo) bool
+	BuyBooks(models.Books, []OrderBookRequest) bool
+	SetDiscount(float64) float64
 }
