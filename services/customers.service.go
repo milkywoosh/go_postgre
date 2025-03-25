6514f34f7839ea9c532b3f85db47a14c1613cdfc
sigma.lukman <auliya.lukman@sigma.co.id>
2025-03-25 21:55:40 +0700

    implement buy books service

--- services/customers.service.go
+++ services/customers.service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/milkyway/gin_beginer/models"
 	"github.com/milkyway/gin_beginer/repositories"
@@ -22,4 +23,32 @@ func (cs CustomerService) GetInfoByRegistry(ctx context.Context, cust_registry s
 	}
 
 	return result, err
+}
+
+// NOTE: parsing array request from postman
+// NOTE: must be transaction!
+func (cs CustomerService) BuyBooks(ctx context.Context, books_repo repositories.BooksRepo, order_req []OrderBookRequest) ([]models.Books, error) {
+
+	var order_validation []models.Books
+
+	for _, book_info := range order_req {
+
+		id, err := strconv.Atoi(book_info.ID)
+		if err != nil {
+			return order_validation, err
+		}
+
+		result_fetch, err := books_repo.FetchBookByID(ctx, id)
+		if err != nil {
+			return order_validation, err
+		}
+
+		order_validation = append(order_validation, result_fetch)
+	}
+	// harusnya doing transaction here!
+	return order_validation, nil
+}
+
+func (cs CustomerService) SetDiscount(float64) float64 {
+	return 0.25
 }
